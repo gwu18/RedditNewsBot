cef62ebe0dc70d9a1f66dc8249f55751068aec4b
Kevin Young <keiayoun@ucsc.edu>
2022-01-15 23:38:58 -0800

    updated sorbet.py

--- newsBot/sorbet.py
+++ newsBot/sorbet.py
@@ -1,10 +1,17 @@
+import linkScraper
 import praw
 
 r = praw.Reddit('bot1')
 subreddit = r.subreddit("news")
 
-for submission in subreddit.hot(limit = 5):
+for submission in subreddit.hot(limit = 1):
+    top_level_comments = list(submission.comments)
     print("Title: ", submission.title)
     print("Text: ", submission.selftext)
     print("Score: ", submission.score)
     print("---------------------------------\n")
+    for comments in top_level_comments:
+        if "that's awful" in comments.body:
+            print("Comment:", comments.body)
+            print("User:", comments.author)
+            r.redditor(str(comments.author)).message("TEST", "test message from PRAW")
